467d1c39341f24dbb52d297b3ab351722ca2ffbc
J Leenman <jonathan@vassal.nl>
2014-01-07 01:18:42 +0100

    license arrays
    arrayed the licenses and cleaned up the echo code.

--- lib/admin-footer.php
+++ lib/admin-footer.php
@@ -1,42 +1,66 @@
 <?php
-
 /*-----------------------------------------------------------------------------------*/
 /* Customise backend developer & copyright footer */
 /*-----------------------------------------------------------------------------------*/
-	function custom_admin_footer() {
-		$developer = 'VASSAL';
-		$url = 'http://www.vassal.nl';
-		$year = '2013';
+function custom_admin_footer() {
 
-		//define licenses output
-		$ccby = 'License ' .
-			'<a rel="license" href="http://creativecommons.org/licenses/by/3.0/deed" target="_blank">CC-BY 3.0</a> '.
-			'<span class="cc-ttf">cb</span>';
-		$ccbysa = 'License ' .
-			'<a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed" target="_blank">CC-BY-SA 3.0</a> '.
-			'<span class="cc-ttf">cba</span>';
-		$ccbynd = 'License ' .
-			'<a rel="license" href="http://creativecommons.org/licenses/by-nd/3.0/deed" target="_blank">CC-BY-ND 3.0</a> '.
-			'<span class="cc-ttf">cbd</span>';
-		
-		//print information
-		echo 	'<span id="footer-thankyou">' .
-				'Developed by <a href="' . $url . '" target="_blank">' . $developer . '</a> ' . $year . 
-			'</span> &mdash; ' .
-			$ccbysa .
-			'<style scoped>
-				@font-face{
-					font-family:"Creative Commons";
-					src:url(' .
-						plugins_url( '../fonts/cc-icons.ttf' , __FILE__ )
-					. ');
-				}
-				.cc-ttf {
-					font-family:"Creative Commons";
-					font-size:15px;
-					letter-spacing:.2em;
-				}</style>';
-	}
+	//selectable licenses
+	$ccby = array(
+		"CC-BY 3.0", //title
+		"http://creativecommons.org/licenses/by/3.0/deed", //url
+		"cb" //icons
+		);
+	$ccbysa = array(
+		"CC-BY-SA 3.0", //title
+		"http://creativecommons.org/licenses/by-sa/3.0/deed", //url
+		"cba"//icons
+		);
+	$ccbynd = array(
+		"CC-BY-ND 3.0", //title
+		"http://creativecommons.org/licenses/by-nd/3.0/deed", //url
+		"cbd"//icons
+		);
+
+	// OPTIONS
+	$developer = 'VASSAL'; //developer name
+	$url = 'http://www.vassal.nl'; //developer url
+	$year = '2014'; //year of delivery
+	$cc = $ccbysa; //select lisence here
+
+	//compose footer
+	$thankyou = '<span id="footer-thankyou">' . 
+					'Developed by ' .
+					'<a href="' . $url . '" target="_blank">' . 
+						$developer . 
+					'</a> ' . 
+					$year. 
+				'</span>';
+	$license = 'License ' .
+					'<a rel="license" 
+						href="'.$cc[1].'" target="_blank">'. //url
+							$cc[0]. //title
+					'</a> '.
+					'<span class="cc-ttf">'.
+						$cc[2]. //icon
+					'</span>';
+	$style = '<style scoped>
+					@font-face{
+						font-family:"Creative Commons";
+						src:url(' .
+							plugins_url( '../fonts/cc-icons.ttf' , __FILE__ )
+						. ');
+					}
+					.cc-ttf {
+						font-family:"Creative Commons";
+						font-size:15px;
+						letter-spacing:.2em;
+					}
+				</style>';
+
+	//print information
+	echo $thankyou  . ' &mdash; ' . $license . $style;
+
+}
 add_filter('admin_footer_text', 'custom_admin_footer');
 
 ?>
