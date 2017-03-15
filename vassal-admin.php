fca6e18587f432827c94140a7b71c875c158d4a9
J Leenman <jonathan@vassal.nl>
2017-03-15 23:58:24 +0100

    removed sidebar

--- vassal-admin.php
+++ vassal-admin.php
@@ -3,9 +3,9 @@
 Plugin Name:         VASSAL ADMIN
 Description:         Customise and rebrand the Wordpress Admin
 Author:             Jonathan Leenman
-Author URI:         http://www.vassal.nl
+Author URI:         https://github.com/jleenman/
 GitHub Plugin URI:	https://github.com/jleenman/vassal-admin
-Version:			1.0.6
+Version:			1.0.7
 
 License:            Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
 License URI:        http://creativecommons.org/licenses/by-sa/4.0/
@@ -13,7 +13,7 @@ License URI:        http://creativecommons.org/licenses/by-sa/4.0/
 
 require_once( plugin_dir_path(__FILE__) . 'lib/admin-rebrand.php' );
 require_once( plugin_dir_path(__FILE__) . 'lib/admin-bar.php' );
-require_once( plugin_dir_path(__FILE__) . 'lib/admin-sidebar.php' );
+//require_once( plugin_dir_path(__FILE__) . 'lib/admin-sidebar.php' );
 require_once( plugin_dir_path(__FILE__) . 'lib/admin-login.php' );
 require_once( plugin_dir_path(__FILE__) . 'lib/admin-footer.php' );
 
