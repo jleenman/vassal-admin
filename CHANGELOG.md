225c30660dec99b16fa8e808eea368fe3619d0dc
J Leenman <jonathan@vassal.nl>
2017-05-09 00:47:05 +0200

    tiny fix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+#### 1.1.1
+* changelog fix
+
 #### 1.1.0
 * renamed readme for github-updater to display in WP admin
 * added changelog for github-updater to display in WP admin
--- vassal-admin.php
+++ vassal-admin.php
@@ -5,7 +5,7 @@ Description:         Customise and rebrand the Wordpress Admin
 Author:             Jonathan Leenman
 Author URI:         https://github.com/jleenman/
 GitHub Plugin URI:	https://github.com/jleenman/vassal-admin
-Version:			1.1.0
+Version:			1.1.1
 
 License:            Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
 License URI:        http://creativecommons.org/licenses/by-sa/4.0/
@@ -13,8 +13,8 @@ License URI:        http://creativecommons.org/licenses/by-sa/4.0/
 
 require_once( plugin_dir_path(__FILE__) . 'lib/admin-rebrand.php' );
 require_once( plugin_dir_path(__FILE__) . 'lib/admin-bar.php' );
-//require_once( plugin_dir_path(__FILE__) . 'lib/admin-sidebar.php' );
 require_once( plugin_dir_path(__FILE__) . 'lib/admin-login.php' );
 require_once( plugin_dir_path(__FILE__) . 'lib/admin-footer.php' );
+//require_once( plugin_dir_path(__FILE__) . 'lib/admin-sidebar.php' );
 
 ?>
