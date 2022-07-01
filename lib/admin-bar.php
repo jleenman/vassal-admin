582c0169f3b3d93466c545c29cb5a8574e1ebe94
jleenman <jonathan@vassal.nl>
2022-07-01 12:00:55 +0200

    2.0.7 -> show admin bar for all users

--- lib/admin-bar.php
+++ lib/admin-bar.php
@@ -43,11 +43,11 @@ add_filter('gettext', 'vssl_welcome_message', 10, 3);
 
 
 /*source: https://www.wpbeginner.com/wp-tutorials/how-to-disable-wordpress-admin-bar-for-all-users-except-administrators/ */
-add_action('after_setup_theme', 'remove_admin_bar');
-function remove_admin_bar() {
-  if (!current_user_can('administrator') && !is_admin()) {
-    show_admin_bar(false);
-  }
-}
+// add_action('after_setup_theme', 'remove_admin_bar');
+// function remove_admin_bar() {
+//   if (!current_user_can('administrator') && !is_admin()) {
+//     show_admin_bar(false);
+//   }
+// }
 
 ?>
